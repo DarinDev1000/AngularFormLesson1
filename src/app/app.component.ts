68d070acdc0aaea96075beec4de725cccc4b6338
Darin <darindev1000@gmail.com>
2019-07-08 01:06:31 -0700

    Add FormGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularFormLesson1';
   accountType = ['Personal', 'Business'];
+  signupForm: FormGroup;
 }
