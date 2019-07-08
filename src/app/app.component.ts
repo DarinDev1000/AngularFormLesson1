07279cba0f4b8845deb6cae5daa9c953ecc3e0e3
Darin <darindev1000@gmail.com>
2019-07-08 01:24:49 -0700

    Add Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +13,14 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'accountType': new FormControl('Personal'),
-      'name': new FormControl(null),
-      'email': new FormControl(null),
+      // Do not call the Validators. Only pass the reference
+      'accountType': new FormControl('Personal', Validators.required),
+      'name': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
       'streetAddress': new FormControl(null),
       'city': new FormControl(null),
-      'country': new FormControl(null),
-      'zipCode': new FormControl(null)
+      'country': new FormControl(null, Validators.required),
+      'zipCode': new FormControl(null, Validators.pattern("[0-9]*"))
     });
   }
 
