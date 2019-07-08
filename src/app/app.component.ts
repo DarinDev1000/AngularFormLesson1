5e4a4c8da90f5970b97fb268a6a04fce809786a5
Darin <darindev1000@gmail.com>
2019-07-08 01:08:47 -0700

    Initialize formGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,7 @@ export class AppComponent implements OnInit {
   accountType = ['Personal', 'Business'];
   signupForm: FormGroup;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.signupForm = new FormGroup({});
+  }
 }
